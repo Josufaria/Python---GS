a9f6ce736e0cf5bb9d0597f1cd0668199b154d62
Josué Faria <josufaria18@gmail.com>
2025-11-13 12:40:55 -0300

    Adicionando o cabeçalho

---
+++ gs.py
@@ -0,0 +1,6 @@
+# =====================================================================
+# Projeto: Monitoramento de Bem-Estar e Requalificação Profissional
+# Integrantes:
+# - Nome: Josué Faria da Silva – RM: 563819
+# - Nome: Julia Schiavi – RM: 562418
+# =====================================================================
