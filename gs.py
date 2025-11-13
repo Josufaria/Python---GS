d2aaa7106ee21244c8ef785cbc3ea16b10bd82d3
Josué Faria <josufaria18@gmail.com>
2025-11-13 12:49:45 -0300

    Começando a primeira parte da estrutura do código

--- gs.py
+++ gs.py
@@ -4,3 +4,14 @@
 # - Nome: Josué Faria da Silva – RM: 563819
 # - Nome: Julia Schiavi – RM: 562418
 # =====================================================================
+
+def avaliar_bem_estar(estresse, sono, atividade):
+
+    if estresse >= 7 and sono < 6:
+        return "ALTO"
+
+    elif estresse >= 5 or sono < 7:
+        return "MÉDIO"
+    
+    else:
+        return "BAIXO"
